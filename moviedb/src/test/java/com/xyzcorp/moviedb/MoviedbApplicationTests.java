d2351e5eace67971f57e305a8aab2d6418bc9349
Soumyabrata Purakayastha <s.purakayastha@accenture.com>
2020-12-09 00:46:58 +0530

    Adding movieDb

---
+++ moviedb/src/test/java/com/xyzcorp/moviedb/MoviedbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xyzcorp.moviedb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviedbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
